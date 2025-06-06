26a1069cf13f0ba2a0db625e47b4ac7d8f52d837
Bruno Capitelli <bruno.nitro48@gmail.com>
2025-06-06 16:56:02 -0300

    chore: minor

--- src/steganography/ports/image_codec_service_port.go
+++ src/steganography/ports/image_codec_service_port.go
@@ -3,9 +3,9 @@ package steganography
 import "image"
 
 type ImageCodecServicePort interface {
-	// Encode encodes msg within img pixels. It does not alter the original image, but returns an encoded version.
+	// Encode encodes msg within img pixels. It does not alter the original image, but returns an encoded version instead.
 	Encode(img image.Image, msg string) *image.RGBA
 
-	// Decode decodes any hidden messge within img pixels. It returns an error if none is found.
+	// Decode decodes any hidden message within img pixels. It returns an error if none is found.
 	Decode(img image.Image) (string, error)
 }
