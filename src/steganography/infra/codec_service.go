f6b380b42bd0edd2ccae44db8d68769bc72d685c
Bruno Capitelli <bruno.nitro48@gmail.com>
2025-06-07 10:46:31 -0300

    refactor: abstracting funcs to codec service

--- src/steganography/infra/codec_service.go
+++ src/steganography/infra/codec_service.go
@@ -1,5 +1,7 @@
 package steganography
 
+import "errors"
+
 type CodecService struct {
 }
 
@@ -7,22 +9,59 @@ func NewCodecService() *CodecService {
 	return &CodecService{}
 }
 
-func (s *CodecService) ByteToBits(b byte) []uint8 {
+func (s *CodecService) ByteToBits(b byte, msbFirst bool) []uint8 {
 	bits := make([]uint8, 8)
 
-	for i := 7; i >= 0; i-- {
-		bits[7-i] = (b >> i) & 1
+	for i := range 8 {
+		bit := (b >> i) & 1
+
+		if msbFirst {
+			bits[7-i] = bit
+		} else {
+			bits[i] = bit
+		}
 	}
 
 	return bits
 }
 
-func (s *CodecService) BitsToByte(bits []uint8) byte {
+func (s *CodecService) BitsToByte(bits []uint8, msbFirst bool) byte {
 	var b byte
 
 	for i := range 8 {
-		b |= bits[i] << (7 - i)
+		if msbFirst {
+			b |= bits[i] << (7 - i)
+		} else {
+			b |= bits[i] << i
+		}
 	}
 
 	return b
+}
+
+func (s *CodecService) StringToBits(str string, msbFirst bool) []uint8 {
+	bits := []uint8{}
+
+	for i := range len(str) {
+		bits = append(bits, s.ByteToBits(str[i], msbFirst)...)
+	}
+
+	return bits
+}
+
+func (s *CodecService) BitsToString(bits []uint8, msbFirst bool) (string, error) {
+	if len(bits) < 8 {
+		return "", errors.New("provided slice has less than 8 bits")
+	}
+	if len(bits)%8 != 0 {
+		bits = bits[:len(bits)-len(bits)%8]
+	}
+
+	bytes := make([]byte, len(bits)/8)
+
+	for i := range len(bytes) {
+		bytes[i] = s.BitsToByte(bits[i*8:(i+1)*8], msbFirst)
+	}
+
+	return string(bytes), nil
 }
--- src/steganography/ports/codec_service_port.go
+++ src/steganography/ports/codec_service_port.go
@@ -1,9 +1,15 @@
 package steganography
 
 type CodecServicePort interface {
-	// ByteToBits converts given byte to a 8-bit slice
-	ByteToBits(b byte) []uint8
+	// ByteToBits converts given byte to a 8-bit slice. If msbFirst is true, uses MSB-first order; uses LSB-first otherwise
+	ByteToBits(b byte, msbFirst bool) []uint8
 
-	// BitsToByte converts given 8-bit slice to a byte
-	BitsToByte(bits []uint8) byte
+	// BitsToByte converts given 8-bit slice to a byte. If msbFirst is true, uses MSB-first order; uses LSB-first otherwise
+	BitsToByte(bits []uint8, msbFirst bool) byte
+
+	// StringToBits converts given string to 8-bit slice. If msbFirst is true, uses MSB-first order; uses LSB-first otherwise
+	StringToBits(str string, msbFirst bool) []uint8
+
+	// BitsToString converts bit slice to string, ignoring any extra bits. If msbFirst is true, uses MSB-first order; uses LSB-first otherwise. Returns an error if len(bits) < 8.
+	BitsToString(bits []uint8, msbFirst bool) (string, error)
 }
